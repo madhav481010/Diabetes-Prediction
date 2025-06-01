261c7b93ce3398d9020b3d862174358f5017bd1b
madhav481010 <madhav.cd22@bmsce.ac.in>
2025-06-01 20:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 with open('best_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-with open('model_metrics.json', 'rb') as f:
+with open('model_metrics.json', 'r') as f:
     metrics_data = json.load(f)
 
 # Prepare model cards
