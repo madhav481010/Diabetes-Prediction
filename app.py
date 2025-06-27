d06e3b13e2f2d85b51b4fd53a59c3a0d186837cf
madhav481010 <madhav.cd22@bmsce.ac.in>
2025-06-27 23:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Load the trained model
 with open('best_model.pkl', 'rb') as f:
